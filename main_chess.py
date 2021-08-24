38cb5c842a594b9b1e08aa1615d8abaa755040c1
Niklas Frondorf <niklas.frondorf@web.de>
2021-08-24 14:52:29 +0200

    added tile drawing

--- main_chess.py
+++ main_chess.py
@@ -37,115 +37,19 @@ def start_game():
 
 
 def draw_tiles():
-    pass
+    width = screen.get_width() / 8
+    height = screen.get_height() / 8
+    for i in range(8):
+        for j in range(8):
+            if (i + j) % 2 == 0:
+                color = (235, 209, 185)
+            else:
+                color = (162, 110, 91)
+            pygame.draw.rect(screen, color, pygame.Rect(i * width, j * height, width, height))
 
 
 def draw_pieces():
     pass
-
-
-def move_entity(entity, pressed_keys):
-    distance = 10
-
-    if len(pressed_keys) > 0:
-        print(pressed_keys)
-
-    # only 'w' pressed
-    if len(pressed_keys) == 1 and pressed_keys[0] == "w":
-        if entity.pos[1] <= distance:
-            entity.pos = (entity.pos[0], 0)
-        else:
-            entity.pos = (entity.pos[0], entity.pos[1] - distance)
-
-    # only 'a' pressed
-    if len(pressed_keys) == 1 and pressed_keys[0] == "a":
-        if entity.pos[0] <= distance:
-            entity.pos = (0, entity.pos[1])
-        else:
-            entity.pos = (entity.pos[0] - distance, entity.pos[1])
-
-    # only 's' pressed
-    if len(pressed_keys) == 1 and pressed_keys[0] == "s":
-        if entity.pos[1] + entity.data.get_height() >= screen.get_height() - distance:
-            entity.pos = (entity.pos[0], screen.get_height() - entity.data.get_height())
-        else:
-            entity.pos = (entity.pos[0], entity.pos[1] + distance)
-
-    # only 'd' pressed
-    if len(pressed_keys) == 1 and pressed_keys[0] == "d":
-        if entity.pos[0] + entity.data.get_width() >= screen.get_width() - distance:
-            entity.pos = (screen.get_width() - entity.data.get_width(), entity.pos[1])
-        else:
-            entity.pos = (entity.pos[0] + distance, entity.pos[1])
-
-    # 'w' and 'a' pressed
-    if len(pressed_keys) == 2 and "w" in pressed_keys and "a" in pressed_keys:
-        if entity.pos[1] <= distance:
-            entity.pos = (entity.pos[0], 0)
-        else:
-            entity.pos = (entity.pos[0], entity.pos[1] - distance / np.sqrt(2))
-
-        if entity.pos[0] <= distance:
-            entity.pos = (0, entity.pos[1])
-        else:
-            entity.pos = (entity.pos[0] - distance / np.sqrt(2), entity.pos[1])
-
-    # 'w' and 'd' pressed
-    if len(pressed_keys) == 2 and "w" in pressed_keys and "d" in pressed_keys:
-        if entity.pos[1] <= distance:
-            entity.pos = (entity.pos[0], 0)
-        else:
-            entity.pos = (entity.pos[0], entity.pos[1] - distance / np.sqrt(2))
-
-        if entity.pos[0] + entity.data.get_width() >= screen.get_width() - distance:
-            entity.pos = (screen.get_width() - entity.data.get_width(), entity.pos[1])
-        else:
-            entity.pos = (entity.pos[0] + distance / np.sqrt(2), entity.pos[1])
-
-    # 's' and 'a' pressed
-    if len(pressed_keys) == 2 and "s" in pressed_keys and "a" in pressed_keys:
-        if entity.pos[1] + entity.data.get_height() >= screen.get_height() - distance:
-            entity.pos = (entity.pos[0], screen.get_height() - entity.data.get_height())
-        else:
-            entity.pos = (entity.pos[0], entity.pos[1] + distance / np.sqrt(2))
-
-        if entity.pos[0] <= distance:
-            entity.pos = (0, entity.pos[1])
-        else:
-            entity.pos = (entity.pos[0] - distance / np.sqrt(2), entity.pos[1])
-
-    # 's' and 'd' pressed
-    if len(pressed_keys) == 2 and "s" in pressed_keys and "d" in pressed_keys:
-        if entity.pos[1] + entity.data.get_height() >= screen.get_height() - distance:
-            entity.pos = (entity.pos[0], screen.get_height() - entity.data.get_height())
-        else:
-            entity.pos = (entity.pos[0], entity.pos[1] + distance / np.sqrt(2))
-
-        if entity.pos[0] + entity.data.get_width() >= screen.get_width() - distance:
-            entity.pos = (screen.get_width() - entity.data.get_width(), entity.pos[1])
-        else:
-            entity.pos = (entity.pos[0] + distance / np.sqrt(2), entity.pos[1])
-
-
-def move_entity_to_mouse(entity):
-    distance = 10
-    e_x, e_y = entity.pos[0], entity.pos[1]
-    m_x, m_y = pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]
-
-    angle = math.atan2(m_y - e_y, m_x - e_x)
-    e_x += distance * np.cos(angle)
-    if e_x < distance:
-        e_x = 1
-    elif e_x + entity.data.get_width() >= screen.get_width():
-        e_x = screen.get_width() - entity.data.get_width()
-
-    e_y += distance * np.sin(angle)
-    if e_y < distance:
-        e_y = 1
-    elif e_y + entity.data.get_height() >= screen.get_height():
-        e_y = screen.get_height() - entity.data.get_height()
-
-    entity.pos = (e_x, e_y)
 
 
 def get_pressed_keys():
@@ -163,12 +67,6 @@ def get_pressed_keys():
         key_list.append("space")
 
     return key_list
-
-
-def update_fps():
-    fps = str(int(clock.get_fps()))
-    fps_text = font.render(fps, 1, pygame.Color("coral"))
-    screen.blit(fps_text, (screen.get_width() - 0.5 * variables.FONT_SIZE * len(fps), 0))
 
 
 if __name__ == '__main__':
